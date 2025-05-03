91054c25c22d4068bf5fbbbe2a0696277517d4ba
augenfrosch <augenfrosch@okonnek.name>
2025-05-03 18:38:26 +0200

    Make item name label consistent with recipe label

--- src/widgets/item_name_label.rs
+++ src/widgets/item_name_label.rs
@@ -16,10 +16,10 @@ impl ItemNameLabel {
 
 impl egui::Widget for ItemNameLabel {
     fn ui(self, ui: &mut egui::Ui) -> egui::Response {
-        let id = egui::Id::new(ui.id().value() ^ u64::from(self.item_id));
+        let id = ui.id().with(self.item_id);
 
         let response = if ui.ctx().animate_bool_with_time(id, false, 0.25) == 0.0 {
-            ui.add(egui::Label::new(egui::RichText::new(&self.text)).sense(egui::Sense::click()))
+            ui.add(egui::Label::new(egui::RichText::new(&self.text)).sense(egui::Sense::CLICK))
         } else {
             ui.add(
                 egui::Label::new(
@@ -33,7 +33,6 @@ impl egui::Widget for ItemNameLabel {
             if ui.input(|i| i.key_pressed(egui::Key::Escape)) {
                 ui.close();
             }
-            let mut selection_made = false;
             if ui.button("Copy item name").clicked() {
                 let copy_item_name = self
                     .text
@@ -41,16 +40,6 @@ impl egui::Widget for ItemNameLabel {
                     .to_string();
                 ui.ctx().copy_text(copy_item_name);
                 ui.close();
-                selection_made = true;
-            }
-            ui.separator();
-            if ui.button("Copy item ID").clicked() {
-                ui.ctx().copy_text(self.item_id.to_string());
-                ui.close();
-                selection_made = true;
-            }
-
-            if selection_made {
                 ui.ctx().animate_bool_with_time(id, true, 0.0);
             }
         });
